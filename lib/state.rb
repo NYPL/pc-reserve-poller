3de688f1515bc289efd4320e8d0530d073924648
danamansana <danielappel@nypl.org>
2022-04-15 14:05:29 -0400

    Add quotes to pcrDateTime in state

--- lib/state.rb
+++ lib/state.rb
@@ -34,7 +34,7 @@ class State
   def self.extract_state_from_db_response (db_response)
     last_element = db_response.lazy.drop(db_response.count  - 1).first
     last_key = last_element["pcrKey"]
-    last_date_time = last_element["pcrDateTime"]
+    last_date_time = "'#{last_element["pcrDateTime"]}'"
     $logger.error('Missing pcrKey or date', db_response: db_response.to_a) if !last_key || !last_date_time
     { "cr_key" => last_key, "pcr_date_time" => last_date_time }
   end
